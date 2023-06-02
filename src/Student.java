317d4edd0b7eb39cc94f237dd4252caffc3e8ead
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-06-02 22:37:22 +0200

    added getters and setters to the student class

--- src/Student.java
+++ src/Student.java
@@ -1,7 +1,38 @@
 public class Student {
+    // All the information every student should have.
     private int studentID;
     private int contactInformation;
     private String enrollmentStatus;
 
-    
+
+    public Student(int studentID, int contactInformation,
+                   String enrollmentStatus){
+        this.studentID = studentID;
+        this.contactInformation = contactInformation;
+        this.enrollmentStatus = enrollmentStatus;
+    }
+
+    // To get and set the studentID.
+    public void set_studentID(int newStudentID){
+        studentID = newStudentID;
+    }
+    public int get_studentID(){
+        return studentID;
+    }
+
+    // To get and set the Contact Information.
+    public void set_contactInformation(int newContactInformation){
+        contactInformation = newContactInformation;
+    }
+    public int get_contactInformation(){
+        return contactInformation;
+    }
+
+    // To get and set the enrollment status.
+    public void set_enrollmentStatus(String newEnrollmentStatus){
+        enrollmentStatus = newEnrollmentStatus;
+    }
+    public String get_enrollmentStatus(){
+        return enrollmentStatus;
+    }
 }

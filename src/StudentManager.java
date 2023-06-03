34e3fe0c626d691b8994eae7ae9be7e71cacaee7
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-06-03 08:24:01 +0200

    creating the studentList inside of a new class

---
+++ src/StudentManager.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class StudentManager {
+    private ArrayList<Student> studentList;
+
+    public StudentManager(){
+        studentList = new ArrayList<>();
+        
+    }
+}

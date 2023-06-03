a1206ab98ac72eea0736d39ec85bf1984b2cc70b
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-06-03 08:26:38 +0200

    did the same code just in a separate class

--- src/StudentManager.java
+++ src/StudentManager.java
@@ -1,10 +1,33 @@
 import java.util.ArrayList;
 
 public class StudentManager {
+
+    // An ArrayList is a dynamic data structure. So we use that to store our students.
     private ArrayList<Student> studentList;
 
     public StudentManager(){
         studentList = new ArrayList<>();
-        
+
+    }
+
+    public void addStudent(Student student){
+        studentList.add(student);
+    }
+
+    // This method searches through our ArrayList for the student we want to delete.
+    public void deleteStudent(int studentIDToRemove){
+        for(int i = 0; i < studentList.size(); i++){
+            // We are using the variable "student" to check if the id matches with our students ID.
+            // We can retrieve the "Student" object at each index
+            Student student = studentList.get(i);
+
+            if (student.get_studentID() == studentIDToRemove){
+                studentList.remove(i);
+                System.out.println("Student with the ID " + studentIDToRemove + " got removed");
+                // Exit the loop if we found the student.
+                return;
+            }
+        }
+        System.out.println("Student with ID " + studentIDToRemove + " was not found");
     }
 }

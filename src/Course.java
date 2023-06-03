4b8bb200db086a77386cb31676a6fddf992de65a
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-06-03 08:45:57 +0200

    added Course class

---
+++ src/Course.java
@@ -0,0 +1,14 @@
+public class Course {
+    private String courseName;
+    private String code;
+    private int creditHours;
+
+    public Course(String courseName, String code, int creditHours){
+        this.courseName = courseName;
+        this.code = code;
+        this.creditHours = creditHours;
+
+    }
+    
+
+}
